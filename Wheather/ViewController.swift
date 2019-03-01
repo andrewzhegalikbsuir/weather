0fab7dce8e3c02cdd80f102d6b93b779fa6693d4
andrewzhegalik <andrew.zhegalik@ifeelyou.io>
2019-03-01 13:48:13 +0300

    Initial Commit

---
+++ Wheather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wheather
+//
+//  Created by Andrew Zhegalik on 3/1/19.
+//  Copyright © 2019 Andrew Zhegalik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
