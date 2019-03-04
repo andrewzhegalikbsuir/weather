bf641c157137a35013ccd9c1e32af7c7bfa3e826
andrewzhegalik <andrew.zhegalik@ifeelyou.io>
2019-03-04 16:57:10 +0300

    first commit

---
+++ Wheather/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Api.swift
+//  Wheather
+//
+//  Created by Andrew Zhegalik on 3/1/19.
+//  Copyright © 2019 Andrew Zhegalik. All rights reserved.
+//
+
+import Foundation
+
+
+struct API {
+    let apiKey = "8c43cb3b8afcf4ddd236b73ca8c1890d"
+    let apiURL = 
+}
---
+++ Wheather/LondonViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LondonViewController.swift
+//  Wheather
+//
+//  Created by Andrew Zhegalik on 3/1/19.
+//  Copyright © 2019 Andrew Zhegalik. All rights reserved.
+//
+
+import Foundation
